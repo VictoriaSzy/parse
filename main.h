41cd47cfa4ee31e619cb558a0f93c1884bbdecc1
VictoriaSzy <vszymanowski00@stuy.edu>
2019-11-26 19:53:00 -0500

    Created file with header and what I included

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <dirent.h>
+#include <signal.h>
+
+char ** parse_args(char * line) ;
